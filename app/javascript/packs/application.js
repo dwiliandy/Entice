775b9304e9c1b0a13206dc1528a5146323cdf92f
Andy Marthin <mr.andymarthin@gmail.com>
2020-12-29 21:22:00 +0700

    Fix: jquery ujs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,11 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+import {} from 'jquery-ujs'
 
-import "../stylesheets/application"
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
+// import "../stylesheets/application"
+// import 'core-js/stable'
+// import 'regenerator-runtime/runtime'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
