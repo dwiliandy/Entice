38d1a0f0bcdabd0e7063cb077ff97910d08ec77f
Andy Marthin <mr.andymarthin@gmail.com>
2020-12-31 04:45:59 +0700

    Fix: featured js

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -12,7 +12,14 @@
 $(document).on('turbolinks:load', function() {
 
 
-
+    $('.featured__controls li').on('click', function () {
+        $('.featured__controls li').removeClass('active');
+        $(this).addClass('active');
+    });
+    if ($('.featured__filter').length > 0) {
+        var containerEl = document.querySelector('.featured__filter');
+        var mixer = mixitup(containerEl);
+    }
     /*------------------
         Background Set
     --------------------*/
