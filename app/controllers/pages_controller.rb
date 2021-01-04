33259bfea5da27d4f378ec444a9c2e8a3524b339
Omega Sekeon <dwiliandy@gmail.com>
2021-01-04 21:28:38 +0800

    add data backend to pages and product info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 before_action :authenticate_user!, except: [:index]
 	def index
+		@products = Product.all
 		
 	end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,14 @@
 class ProductsController < ApplicationController
+  before_action :set_product
   def show
   end
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:price, :description, :name, :image, :status, :stock, :region_id)
+    end
 end
