572b24c5424a623aea595eeb9ce0dedb0ac6b302
Omega Sekeon <dwiliandy@gmail.com>
2021-01-07 20:58:38 +0800

    add to heroku

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,18 +2,21 @@
 #
 # Table name: carts
 #
-#  id         :bigint           not null, primary key
-#  price      :float
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint           not null
+#  id             :bigint           not null, primary key
+#  price          :float
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  transaction_id :bigint           not null
+#  user_id        :bigint           not null
 #
 # Indexes
 #
-#  index_carts_on_user_id  (user_id)
+#  index_carts_on_transaction_id  (transaction_id)
+#  index_carts_on_user_id         (user_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (transaction_id => transactions.id)
 #  fk_rails_...  (user_id => users.id)
 #
 class Cart < ApplicationRecord
