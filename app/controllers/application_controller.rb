34ba8530ee5cfc02cfe8c7ae86782ccf5e478d65
Omega Sekeon <dwiliandy@gmail.com>
2021-03-03 18:34:20 +0800

    fix sign in redirect back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,36 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
 
 
-  def after_sign_in_path_for(resource)
+
+  def after_sign_in_path_for(resource_or_scope)
     # check for the class of the object to determine what type it is
     case resource.role
     when "admin"
       admins_pages_index_path
     when "customer"
-      root_path
+      stored_location_for(resource_or_scope) || super
     else
       root_path
     end
   end
 
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
 
   protected
+
+  
 
        def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up) {
