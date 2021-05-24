bea9076488fc81172431cccd44e75281069a9c86
Daniel <danielgunawann1997@gmail.com>
2021-05-24 16:11:30 +0700

    move search to below slide show

--- db/schema.rb
+++ db/schema.rb
@@ -46,10 +46,10 @@ ActiveRecord::Schema.define(version: 2021_05_24_070919) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
-    t.bigint "user_id", null: false
     t.bigint "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["product_id"], name: "index_comments_on_product_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
