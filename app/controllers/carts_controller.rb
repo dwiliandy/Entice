4e9e354c1089de6cea093ead5e91d4f6dba0f382
Omega Sekeon <dwiliandy@gmail.com>
2021-07-28 08:10:15 +0800

    update service charge

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,6 +4,15 @@ before_action :set_cart, only: [:create_transaction]
   def create_transaction
     if @cart.order.present?
       @trans = @cart.order
+      @region = @cart.products.pluck(:region_id).max
+      if (1..282).include? (@region)
+        @service_charge_id = ServiceCharge.first.id
+      elsif (283..451)
+        @service_charge_id = ServiceCharge.second.id
+      else
+        @service_charge_id = ServiceCharge.last.id
+      end
+      @trans.update(service_charge_id: @service_charge_id)
       if params[:coupon].present?
         @trans.update(coupon_id: params[:coupon].to_i)
       else params[:coupon].blank?
@@ -11,7 +20,14 @@ before_action :set_cart, only: [:create_transaction]
       end
     else
       @status = 'pending'
-      @service_charge_id = ServiceCharge.first.id
+      @region = @cart.products.pluck(:region_id).max
+      if (1..282).include? (@region)
+        @service_charge_id = ServiceCharge.first.id
+      elsif (452..514)
+        @service_charge_id = ServiceCharge.last.id
+      else
+        @service_charge_id = ServiceCharge.second.id
+      end
       @coupon_id = @coupon_id = Coupon.find(params[:coupon]).id if params[:coupon].present?
       @trans = Order.create(status:@status, cart: @cart, service_charge_id:@service_charge_id, coupon_id: @coupon_id)
       TransactionDetail.create(order:@trans)
